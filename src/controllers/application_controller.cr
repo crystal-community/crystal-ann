9fdc00d3f7c7859aa33d17606d21df2987eb6af4
Vitalii Elenhaupt <velenhaupt@gmail.com>
2017-07-09 23:12:03 +0300

    Add footer

--- src/controllers/application_controller.cr
+++ src/controllers/application_controller.cr
@@ -1,3 +1,5 @@
 class ApplicationController < Amber::Controller::Base
-  LAYOUT = "application.slang"
+  LAYOUT      = "application.slang"
+  TITLE       = "Crystal [ANN]"
+  DESCRIPTION = "Announce new project, blog post, version update or any other Crystal work."
 end
