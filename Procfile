b5c25fb4233b2ebc2534274b8aaf985bcc50d0fd
V. Elenhaupt <velenhaupt@gmail.com>
2017-07-13 18:20:06 +0300

    Autotweets (#1)
    * Sidekiq to tweet announcements
    
    * Add staging env
    
    * Sidekiq number of workers
    
    * Correct twitter env vars
    
    * Add deployment notes

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: ./app
+worker: ./sidekiq -c 3
--- src/crystal-ann.cr
+++ src/crystal-ann.cr
@@ -1,8 +1,17 @@
 require "amber"
+require "micrate"
+require "pg"
 require "./controllers/**"
 require "./mailers/**"
 require "./models/**"
 require "./views/**"
 require "../config/*"
+
+if AMBER_ENV != "development"
+  puts "Migrating data"
+  Micrate::DB.connection_url = ENV["DATABASE_URL"]?
+  Micrate::Cli.run_up
+  puts "Migration finished"
+end
 
 Amber::Server.instance.run
