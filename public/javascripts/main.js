1aea4710ddb15eaa3eca1e550e4897bfde03143b
Hugo Abonizio <hugo_abonizio@hotmail.com>
2017-07-20 14:34:07 -0300

    Add sign out feature

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,13 +1,32 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   toggleSecondary();
+  signoutListener();
 });
 
-function toggleSecondary(){
+function toggleSecondary() {
   var button = document.getElementsByClassName("secondary-toggle")[0];
   var secondary = document.getElementsByClassName("secondary")[0];
 
   if (secondary && button) {
     button.addEventListener("click", () =>
       secondary.classList.toggle("toggled-on"));
+  }
+}
+
+function signoutListener() {
+  var button = document.getElementById("sign-out-btn");
+  if (button) {
+    button.addEventListener("click", (e) => {
+      e.preventDefault();
+      var form = document.createElement("form");
+      form.method = "POST";
+      form.action = "/sessions";
+      var method = document.createElement("input");
+      method.name = "_method";
+      method.value = "DELETE";
+      form.appendChild(method);
+      document.body.appendChild(form);
+      form.submit();
+    });
   }
 }
