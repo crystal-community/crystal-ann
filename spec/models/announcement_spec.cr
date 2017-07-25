dc9693aa44acb949e3ad335e732a8e246bbab4a1
Hugo Abonizio <hugo_abonizio@hotmail.com>
2017-07-25 09:24:56 -0300

    Update specs

--- spec/models/announcement_spec.cr
+++ spec/models/announcement_spec.cr
@@ -37,10 +37,15 @@ describe Announcement do
   end
 
   describe "#typename" do
-    it "returns the name of the type" do
-      Announcement::TYPES.each do |type, name|
-        expect(announcement(type: type).typename).to eq name
-      end
+    it "returns the properly capitalized type name" do
+      expect(announcement(type: 0).typename).to eq "Blog Post"
+      expect(announcement(type: 1).typename).to eq "Project Update"
+      expect(announcement(type: 2).typename).to eq "Conference"
+      expect(announcement(type: 3).typename).to eq "Meetup"
+      expect(announcement(type: 4).typename).to eq "Podcast"
+      expect(announcement(type: 5).typename).to eq "Screencast"
+      expect(announcement(type: 6).typename).to eq "Video"
+      expect(announcement(type: 7).typename).to eq "Other"
     end
 
     it "raises error if type is wrong" do
@@ -77,36 +82,6 @@ describe Announcement do
     it "returns hash id if id is present" do
       hashid = announcement.tap { |a| a.id = 1_i64 }.hashid
       expect(hashid).to eq "D49Nz"
-    end
-  end
-
-  describe "#typename_underscore" do
-    it "returns the type with underscores" do
-      expect(announcement(type: 0).typename_underscore).to eq "blog_post"
-      expect(announcement(type: 1).typename_underscore).to eq "project_update"
-      expect(announcement(type: 2).typename_underscore).to eq "conference"
-      expect(announcement(type: 3).typename_underscore).to eq "meetup"
-      expect(announcement(type: 4).typename_underscore).to eq "podcast"
-      expect(announcement(type: 5).typename_underscore).to eq "screencast"
-      expect(announcement(type: 6).typename_underscore).to eq "video"
-      expect(announcement(type: 7).typename_underscore).to eq "other"
-    end
-  end
-
-  describe ".reverse_typename_underscore" do
-    it "returns nil if is invalid" do
-      expect(Announcement.reverse_typename_underscore("foo")).to eq nil
-    end
-
-    it "returns the type given a typename_underscore" do
-      expect(Announcement.reverse_typename_underscore("blog_post")).to eq 0
-      expect(Announcement.reverse_typename_underscore("project_update")).to eq 1
-      expect(Announcement.reverse_typename_underscore("conference")).to eq 2
-      expect(Announcement.reverse_typename_underscore("meetup")).to eq 3
-      expect(Announcement.reverse_typename_underscore("podcast")).to eq 4
-      expect(Announcement.reverse_typename_underscore("screencast")).to eq 5
-      expect(Announcement.reverse_typename_underscore("video")).to eq 6
-      expect(Announcement.reverse_typename_underscore("other")).to eq 7
     end
   end
 end
