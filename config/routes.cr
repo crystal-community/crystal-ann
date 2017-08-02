3f5e265e33ba87e7a082947822cd1b13b9d75857
Vitalii Elenhaupt <velenhaupt@gmail.com>
2017-08-02 10:46:51 +0300

    Enable CSRF plug

--- config/routes.cr
+++ config/routes.cr
@@ -2,11 +2,10 @@ Amber::Server.instance.config do |app|
   pipeline :web do
     # Plug is the method to use connect a pipe (middleware)
     # A plug accepts an instance of HTTP::Handler
-    # plug Amber::Pipe::Params.new
     plug Amber::Pipe::Logger.new unless app.env == "test"
     plug Amber::Pipe::Flash.new
     plug Amber::Pipe::Session.new
-    # plug Amber::Pipe::CSRF.new
+    plug Amber::Pipe::CSRF.new
   end
 
   # All static content will run these transformations
