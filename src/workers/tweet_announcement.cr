72bf5c922a033910015866b15ea86af442f99abc
Vitalii Elenhaupt <velenhaupt@gmail.com>
2017-09-19 18:24:26 +0300

    Twitter worker specs with webmock

--- src/workers/tweet_announcement.cr
+++ src/workers/tweet_announcement.cr
@@ -1,4 +1,5 @@
 require "sidekiq"
+
 require "twitter-crystal"
 require "../models/announcement"
 
@@ -6,27 +7,28 @@ module Workers
   class TweetAnnouncement
     include Sidekiq::Worker
 
+    def initialize
+      @twitter_client =
+        Twitter::REST::Client.new ENV.fetch("TWITTER_CONSUMER_KEY", ""),
+          ENV.fetch("TWITTER_CONSUMER_SECRET", ""),
+          ENV.fetch("TWITTER_ACCESS_TOKEN", ""),
+          ENV.fetch("TWITTER_ACCESS_TOKEN_SECRET", "")
+    end
+
     def perform(id : Int64)
       if announcement = Announcement.find(id)
         tweet(announcement)
       end
     end
 
-    def twitter_client
-      @twitter_client ||=
-        Twitter::REST::Client.new ENV["TWITTER_CONSUMER_KEY"],
-          ENV["TWITTER_CONSUMER_SECRET"],
-          ENV["TWITTER_ACCESS_TOKEN"],
-          ENV["TWITTER_ACCESS_TOKEN_SECRET"]
-    end
-
     def tweet(announcement)
       logger.info "Tweeting Announcement ##{announcement.id}"
       status = tweet_template announcement
-      twitter_client.update(status)
+      @twitter_client.update(status)
     rescue e
       logger.error "Unable to tweet Announcement ##{announcement.id} (#{status})"
       logger.error "Reason: #{e.message}"
+      false
     end
 
     def tweet_template(announcement)
