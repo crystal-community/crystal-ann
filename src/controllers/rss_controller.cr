8e4c70de01d266301a6cf03099c6bdac8c5a0962
V. Elenhaupt <velenhaupt@gmail.com>
2018-03-12 20:05:40 +0200

    Change to xml respond_with

--- src/controllers/rss_controller.cr
+++ src/controllers/rss_controller.cr
@@ -2,9 +2,10 @@ class RSSController < Amber::Controller::Base
   include Helpers::TimeAgoHelper
 
   def show
-    response.headers["Content-Type"] = "application/xml"
-
     announcements = Announcement.newest
-    render "rss/show.slang", layout: false
+
+    respond_with do
+      xml render "rss/show.slang", layout: false
+    end
   end
 end
