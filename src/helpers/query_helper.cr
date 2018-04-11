22b3e2dabf13a044436cb631cbaa4649ea5907b2
V. Elenhaupt <velenhaupt@gmail.com>
2018-04-11 17:15:02 +0300

    Avoid params mutation

--- src/helpers/query_helper.cr
+++ src/helpers/query_helper.cr
@@ -1,6 +1,6 @@
 module Helpers::QueryHelper
   def to_query(**query)
-    current_params = params.to_h
+    current_params = params.to_h.dup
     query.each { |k, v| current_params[k.to_s] = v.to_s }
     HTTP::Params.encode current_params
   end
